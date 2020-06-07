c211fb99b7d00d468d82e67f351b51dbebbc030c
jwl-oss <55232713+jwl-oss@users.noreply.github.com>
2020-06-07 23:27:41 +0800

    Add files via upload

---
+++ lab10/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'jwl-odd');
+define('DBPASS', '1772079038jwl.');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
+?>
